7b68bad808445dc9facc5f9786aad831f1375cfe
clubberstake <Gregory.Lang@gmail.com>
2015-01-29 17:25:51 -0500

    some formatting and brought in 'history and purpose'

--- Content/app/js/listscript.js
+++ Content/app/js/listscript.js
@@ -1,12 +1,12 @@
-function listctrl($scope) {    
-    $scope.showChilds = function(index){
+function listctrl($scope) {
+    $scope.showChilds = function (index) {
         $scope.items[index].active = !$scope.items[index].active;
         collapseAnother(index);
     };
 
-    var collapseAnother = function(index){
-        for(var i=0; i<$scope.items.length; i++){
-            if(i!=index){
+    var collapseAnother = function (index) {
+        for (var i = 0; i < $scope.items.length; i++) {
+            if (i != index) {
                 $scope.items[i].active = false;
             }
         }
@@ -16,23 +16,46 @@ function listctrl($scope) {
         {
             name: "About Us",
             subItems: [
-                {name: "History & Purpose", link: "#/placeholder"},
-                {name: "Annual Report", link: "#/aboutus"},
-                {name: "Contact Cotraic", link: ""}
+                {
+                    name: "History & Purpose",
+                    link: "#/historyAndPurpose/historyAndPurpose.html"
+                },
+                {
+                    name: "Annual Report",
+                    link: "#/aboutus"
+                },
+                {
+                    name: "Contact Cotraic",
+                    link: ""
+                }
             ]
         },
         {
             name: "Education",
             subItems: [
-                {name: "Headstart", subItems: [{name: "What is Headstart?"}]},
-                {name: "Early Headstart", subItems: [{name: "What is Early Headstart?"}]},
-                {name: "Pre-K"}
+                {
+                    name: "Headstart",
+                    subItems: [{
+                        name: "What is Headstart?"
+                    }]
+                },
+                {
+                    name: "Early Headstart",
+                    subItems: [{
+                        name: "What is Early Headstart?"
+                    }]
+                },
+                {
+                    name: "Pre-K"
+                }
             ]
         },
         {
             name: "Item3",
             subItems: [
-                {name: "SubItem6"}
+                {
+                    name: "SubItem6"
+                }
             ]
         }
     ];
--- Content/app/js/script.js
+++ Content/app/js/script.js
@@ -3,30 +3,30 @@
 var app = angular.module('bindexample', ['ngRoute'])
 
 .config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider
-    .when('/count', {
-      templateUrl: 'placeholder.html',
-      controller: 'mycontroller'
-    })
-    
-    .when('/aboutus', {
-      templateUrl: 'aboutus.html',
-      controller: 'mycontroller'
-    })
-    
-    
-    .otherwise({
-      redirectTo: '/count'
-    })
+  function ($routeProvider) {
+        $routeProvider
+            .when('/count', {
+                templateUrl: 'placeholder.html',
+                controller: 'mycontroller'
+            })
+
+        .when('/aboutus', {
+            templateUrl: 'aboutus.html',
+            controller: 'mycontroller'
+        })
+
+
+        .otherwise({
+            redirectTo: '/count'
+        })
   }
 ])
 
 .controller('mycontroller', ['$scope',
-  function($scope) {
-    $scope.counter = 1;
-    $scope.click = function() {
-      $scope.counter = parseInt($scope.counter) + 1;
-    }
+  function ($scope) {
+        $scope.counter = 1;
+        $scope.click = function () {
+            $scope.counter = parseInt($scope.counter) + 1;
+        }
   }
 ])
