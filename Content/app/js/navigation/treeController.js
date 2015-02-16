d8b69f822f82e4b597ce83831d9fef9992ada3f4
clubberstake <gregory.lang@gmail.com>
2015-02-16 18:32:40 -0500

    fixing the bad merge with tree controller

--- Content/app/js/navigation/treeController.js
+++ Content/app/js/navigation/treeController.js
@@ -13,54 +13,7 @@ app.directive('treeview', function() {
 app.controller("TreeController", ['$scope', function($scope) {
     this.init = function() {
 
-    };
-<<<<<<< HEAD
-    $scope.tree = [{
-        name: "About Us",
-        link: "#/aboutUs",
-        nodes: [{
-            name: "History & Purpose",
-            link: "#/historyAndPurpose"
-        }, {
-            name: "Annual Report",
-            link: "#/annualReport"
-        }, {
-            name: "Contact Cotraic",
-            link: "#/ContactCotraic"
-        }]
-    }, {
-        name: "Education",
-        nodes: [{
-            name: "Headstart",
-            nodes: [{
-                name: "What is Headstart?",
-                link: "#/whatIsHeadStart"
-            }]
-        }, {
-            name: "Early Headstart",
-            link: "#/EarlyHeadStart",
-            nodes: [{
-                name: "What is Early Headstart?"
-            }]
-        }, {
-            name: "Pre-K",
-            link: "#/preK"
-        }]
-    }, {
-        name: "Assistance Programs",
-        link: "",
-        nodes: [{
-            name: "WIA Program",
-            link: "",
-            nodes: [{
-                name: "WIA Contact",
-                link: "#/WIAContact"
-            }]
-
-        }]
-    }];
-}]);
-=======
+    };    
     $scope.tree = [
         {
             name: "About Us",
@@ -129,4 +82,3 @@ app.controller("TreeController", ['$scope', function($scope) {
         }
     ];
 }]);
->>>>>>> dev
