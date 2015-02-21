907ba3ee251a34e02dd0a3a2c8557f0c48cb92b6
lucci <mcolucci860@gmail.com>
2015-02-20 21:07:42 -0500

    Added scopes to preK and also added tests for preK

--- Content/app/js/preK/preKController.js
+++ Content/app/js/preK/preKController.js
@@ -1,8 +1,9 @@
 app.controller('PreKController', ['$scope',
 
     function ($scope) {
-        this.init = function () {
-
-        };
-        $scope.doSomething = function () {};
+        this.init = function () {};
+        $scope.titletext = "Pre-K";
+        $scope.text = "The Council of Three Rivers American Indian Centers, Inc. Pre-K Counts Program provides high quality pre-kindergarten education for 3 and 4 year olds at no cost to families. It is a two year program for your child.";
+        $scope.copyrightdate = "2014";
+        $scope.copyrighttext = "&copy; " + $scope.copyrightdate + " COTRAIC, Inc.";
 }]);
