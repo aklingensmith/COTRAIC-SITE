0671ee8a8fedfc471b3e7e7061fc555207cd5290
clubberstake <gregory.lang@gmail.com>
2015-02-23 18:52:47 -0500

    removing console log statement I accidentally left in.

--- Content/app/js/aboutUs/aboutUsController.js
+++ Content/app/js/aboutUs/aboutUsController.js
@@ -2,7 +2,6 @@ app.controller('AboutUsController', ['$scope', 'AboutUsService',
 
     function ($scope, AboutUsService) {
         AboutUsService.get().then(function (data) {
-            console.log(data);
             $scope.title = data.data.title;
             $scope.text = data.data.text;
         })
