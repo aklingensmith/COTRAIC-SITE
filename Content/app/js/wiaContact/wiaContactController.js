a534f931235e9e35f200da48a13def62410e3900
lucci <mcolucci860@gmail.com>
2015-03-16 18:20:18 -0400

    Fixed controller for WIA Contact so that it displayed the correct information

--- Content/app/js/wiaContact/wiaContactController.js
+++ Content/app/js/wiaContact/wiaContactController.js
@@ -4,13 +4,20 @@ app.controller('WIAContactController', ['$scope', 'WIAContactService',
         WIAContactService.get().then(function (data) {
             $scope.titletext = data.data.titletext;
             $scope.wpawiacenter = data.data.wpawiacenter;
+            $scope.wpawiacenteremail = data.data.wpawiacenteremail;
             $scope.epawiacenter = data.data.epawiacenter;
+            $scope.epawiacenteremail = data.data.epawiacenteremail;
             $scope.wwvcenter = data.data.wwvcenter;
+            $scope.wwvcenteremail = data.data.wwvcenteremail;
             $scope.fbwiahagerstown = data.data.fbwiahagerstown;
+            $scope.fbwiahagerstownemail = data.data.fbwiahagerstownemail;
             $scope.fbwiabaltimore = data.data.fbwiabaltimore;
+            $scope.fbwiabaltimoreemail = data.data.fbwiabaltimoreemail;
             $scope.lexingtonwia = data.data.lexingtonwia;
+            $scope.lexingtonwiaemail = data.data.lexingtonwiaemail;
             $scope.louisvillewia = data.data.louisvillewia;
-	    $scope.copyrightdate = data.data.copyrightdate;
-	    $scope.copyrighttext = data.data.copyrighttext;
+            $scope.louisvillewiaemail = data.data.louisvillewiaemail;
+            $scope.copyrightdate = data.data.copyrightdate;
+            $scope.copyrighttext = data.data.copyrighttext;
         })
 }]);
