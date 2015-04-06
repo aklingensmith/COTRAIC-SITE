ee0dddd003b856a6c2d39b29a90ff9cc35b2a52f
lucci <mcolucci860@gmail.com>
2015-04-06 18:40:55 -0400

    Deleted 'obolete' folder that I created since it is redundant

--- Content/app/obsolete/js/preKEnrollment/preKEnrollmentController.js
+++
@@ -1,9 +0,0 @@
-app.controller('PreKEnrollmentController', ['$scope', 'PreKEnrollmentService',
-
-    function ($scope, PreKEnrollmentService) {
-        PreKEnrollmentService.get().then(function (data) {
-            $scope.title = data.data.title;
-            $scope.text = data.data.text;
-            $scope.copyrighttext = data.data.copyrighttext;
-        })
-    }]);
