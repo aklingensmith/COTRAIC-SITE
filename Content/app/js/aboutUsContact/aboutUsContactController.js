f3c5e5d7299d134e08d9cab86d00b8392b328bcb
Ken Sandala <KennethSandala@gmail.com>
2015-04-06 22:29:49 -0400

    Add conditional display for email popup (located in json)

--- Content/app/js/aboutUsContact/aboutUsContactController.js
+++ Content/app/js/aboutUsContact/aboutUsContactController.js
@@ -4,7 +4,7 @@ app.controller('AboutUsContactController', ['$scope', 'AboutUsContactService',
         AboutUsContactService.get().then(function (data) {
             $scope.title = data.data.title;
             $scope.oname = data.data.oname;
-	    $scope.onameshort = data.data.onameshort;
+	    	$scope.onameshort = data.data.onameshort;
             $scope.addressLine = data.data.addressLine;
             $scope.cityLine = data.data.cityLine;
             $scope.phoneLine = data.data.phoneLine;
@@ -13,20 +13,18 @@ app.controller('AboutUsContactController', ['$scope', 'AboutUsContactService',
             $scope.contactEmail = data.data.contactEmail;
             $scope.formInfo = data.data.formInfo;
             $scope.formTitle = data.data.formTitle;
+
+			$scope.displayPopup = data.data.displayPopup;
+	    
         })
 
 
         $scope.openEmail = function () {
             $scope.showEmailDialog = !$scope.showEmailDialog;
-		$scope.test = { cname: $scope.contactName, email: $scope.contactEmail};
+			$scope.test = { cname: $scope.contactName, email: $scope.contactEmail};
         };
     }
 ]);
 
-app.directive('modalDialog', function () {
-    return {
-        restrict: 'E',
-        scope: {
-        },
-    };
-});
+
+
