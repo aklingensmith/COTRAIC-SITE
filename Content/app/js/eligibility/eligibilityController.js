90bb97702e9899d2e092ad3248ab168617d8066f
lucci <mcolucci860@gmail.com>
2015-04-13 17:40:15 -0400

    Added information to eligibility and Headstart

--- Content/app/js/eligibility/eligibilityController.js
+++ Content/app/js/eligibility/eligibilityController.js
@@ -4,7 +4,6 @@ app.controller('EligibilityController', ['$scope', 'EligibilityService',
         EligibilityService.get().then(function (data) {
             $scope.titletext = data.data.titletext;
             $scope.text = data.data.text;
-            $scope.copyrighttext = data.data.copyrighttext;
             $scope.link = data.data.link;
         })
     }]);
--- Content/app/js/headStartServices/headStartServicesController.js
+++ Content/app/js/headStartServices/headStartServicesController.js
@@ -4,9 +4,5 @@ app.controller('HeadStartServicesController', ['$scope', 'HeadStartServicesServi
         HeadStartServicesService.get().then(function (data) {
             $scope.title = data.data.title;
             $scope.text = data.data.text;
-            $scope.text2 = data.data.text2;
-            $scope.text3 = data.data.text3;
-            $scope.text4 = data.data.text4;
-            $scope.copyrighttext = data.data.copyrighttext;
         })
     }]);
