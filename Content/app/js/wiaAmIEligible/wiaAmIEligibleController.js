c66f21e66e0b9737175f41465419ae2f2f917ec7
odbrown44 <odbrown44@gmail.com>
2015-04-13 19:27:59 -0400

    I updated wiaAmIEligible  pages html json controller.js and service.js and the css page

--- Content/app/js/wiaAmIEligible/wiaAmIEligibleController.js
+++ Content/app/js/wiaAmIEligible/wiaAmIEligibleController.js
@@ -9,6 +9,14 @@ app.controller('WIAAmIEligibleController', ['$scope', 'WIAAmIEligibleService',
             $scope.three = data.data.three;
             $scope.four = data.data.four;
             $scope.five = data.data.five;
+            $scope.text2 = data.data.text2;
+            $scope.text3 = data.data.text3;
+            $scope.text4 = data.data.text4;
+            $scope.text5 = data.data.text5;
+            $scope.text6 = data.data.text6;
+            $scope.text7 = data.data.text7;
+            $scope.text8 = data.data.text8;
+            $scope.text9 = data.data.text9;
             $scope.copyrighttext = data.data.copyrighttext;
         })
     }]);
