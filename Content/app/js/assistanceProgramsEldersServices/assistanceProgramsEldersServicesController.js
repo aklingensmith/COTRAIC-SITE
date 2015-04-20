fe600facbe6be3df608d4f789fd43640a51da1be
odbrown44 <odbrown44@gmail.com>
2015-04-20 19:32:20 -0400

    I updated wiaServices assistanceProgramsEldersServices wiaAmIEligible and eldersEligiblity and added asstianceProgramsEldersServices.css and updated wiaServices.css

--- Content/app/js/assistanceProgramsEldersServices/assistanceProgramsEldersServicesController.js
+++ Content/app/js/assistanceProgramsEldersServices/assistanceProgramsEldersServicesController.js
@@ -1,15 +1,19 @@
 app.controller('AssistanceProgramsEldersServicesController', ['$scope', 'AssistanceProgramsEldersServicesService',
 
     function ($scope, AssistanceProgramsEldersServicesService) {
-        AssistanceProgramsEldersServicesService.get().then(function(data) {
-            
-        $scope.title = data.data.title;
-        $scope.information = data.data.information;
-        $scope.outReach = data.data.outReach;
-        $scope.socialization = data.data.socialization;
-        $scope.counseling = data.data.counseling;
-        $scope.emergency = data.data.emergency;
-        $scope.copyrighttext = data.data.copyrighttext;
-            
+        AssistanceProgramsEldersServicesService.get().then(function (data) {
+
+            $scope.title = data.data.title;
+            $scope.information = data.data.information;
+            $scope.outReach = data.data.outReach;
+            $scope.socialization = data.data.socialization;
+            $scope.counseling = data.data.counseling;
+            $scope.emergency = data.data.emergency;
+            $scope.bf1 = data.data.bf1;
+            $scope.bf2 = data.data.bf2;
+            $scope.bf3 = data.data.bf3;
+            $scope.bf4 = data.data.bf4;
+            $scope.copyrighttext = data.data.copyrighttext;
+
         })
 }]);
